fb030d0b5df181b8b5fc196fe929b92fe91597c5
lmoreno94 <lmoreno9943@gmail.com>
2025-07-06 16:51:16 -0500

    add heroe card

---
+++ src/components/Heroes/HeroList.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+interface HeroCardProps {
+	id: string;
+	superhero: string;
+	publisher: string;
+	alter_ego: string;
+	first_appearance: string;
+	characters: string;
+}
+
+function HeroList(props: HeroCardProps) {
+    const { id, superhero, alter_ego, first_appearance, characters } = props;
+	return (
+        <div className="col">
+            <div className="card" style={{ maxWidth: '10rem' }}>
+                <img src={`./assets/heroes/${id}.jpg`} className="card-img-top" alt={superhero} />
+                <h5 className="card-header">{superhero}</h5>
+                <div className="card-body">
+                    <p className="card-text">{alter_ego}</p>
+                    {alter_ego !== characters && <p className='card-text'>{characters}</p>}
+                    <p className="text-muted">{first_appearance}</p>
+                    <Link to={`./heroe/${id}`}>
+                        Más...
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeroList;
