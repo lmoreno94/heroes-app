84be84012b437b0bf1a0a9ae560732f1afc1f891
lmoreno94 <lmoreno9943@gmail.com>
2025-07-06 17:15:33 -0500

    fix heroe list

--- src/components/Heroes/HeroList.tsx
+++ src/components/Heroes/HeroList.tsx
@@ -1,22 +1,18 @@
-import { Link } from "react-router-dom";
-import type { Hero } from "../../types/Hero";
+import type { Hero, HeroPublisher } from "../../types/Hero";
+import HeroCard from "./HeroCard";
+import { getHeroesByPublisher } from "../../selectors/getHeroesByIdPublisher";
 
-function HeroList(props: Hero) {
-    const { id, superhero, alter_ego, first_appearance, characters } = props;
-	return (
-        <div className="col">
-            <div className="card" style={{ maxWidth: '10rem' }}>
-                <img src={`./assets/heroes/${id}.jpg`} className="card-img-top" alt={superhero} />
-                <h5 className="card-header">{superhero}</h5>
-                <div className="card-body">
-                    <p className="card-text">{alter_ego}</p>
-                    {alter_ego !== characters && <p className='card-text'>{characters}</p>}
-                    <p className="text-muted">{first_appearance}</p>
-                    <Link to={`./heroe/${id}`}>
-                        Más...
-                    </Link>
-                </div>
-            </div>
+function HeroList(props: HeroPublisher) {
+    const { publisher } = props;
+    const heroes: Hero[] = getHeroesByPublisher(publisher);
+
+    return (
+        <div className='row row-cols-1 row-cols-md-3 g-4 border-dark mb-3 animate__animated animate__bounceInLeft'>
+            {
+                heroes.map(hero => (
+                    <HeroCard key={hero.id} {...hero} />
+                ))
+            }
         </div>
     );
 }
